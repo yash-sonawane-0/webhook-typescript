d16687db4d9f6ea49226447b288f3a9629351787
yash-sonawane-4930 <yash493031@gmail.com>
2022-12-29 00:38:01 +0530

    initial_commit

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,10 @@ import express from "express";
 import { initializeApp } from 'firebase/app'
 import { config } from "./config/config";
 require('dotenv').config(); // good practice to keep secret here
+const firebase = require("firebase");
 
 initializeApp(config.firebaseConfig);
+const db = firebase.firestore();
 
 const app = express();
 
@@ -16,3 +18,35 @@ app.get("/", (req, res) => {
     res.status(200).send("Hello this is webhook");
 });
 
+app.post("/webhook/listen", async (req, res) => {
+    res.status(200).send("Inside webhook/listen");
+    let body_param = req.body;
+    console.log(body_param);
+    if (body_param.object) { // if it has something
+        let thread = body_param.data.object.thread;
+        let id = body_param.data.object.id;
+        let member = body_param.member;
+        let body = body_param.data.object.text;
+        let createdAt = body_param.created_at;
+        let media = body_param.data.object.attachments;
+
+        const snapshot = db.collection('Tokens').doc(member);
+        const doc = await snapshot.get();
+        if (!doc.exists) {
+            console.log('No such document in Firebase!!');
+        } else {
+            console.log("Thread : body -> " + thread + " : " + body);
+
+            let token = doc.data().Token;
+
+            if (member === body_param.data.object.sender) { // dont send notification for user's own messages
+            } else {
+                console.log("User token is : " + token);
+            }
+        }
+        res.sendStatus(200);
+    } else {
+        res.sendStatus(200);
+    }
+
+});
